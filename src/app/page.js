c4a354dec7144b97f5a7c98d216237fce49c295c
vishal <vishalchanda2018@gmail.com>
2025-06-18 11:11:59 +0530

    pupilo delivers file added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,12 @@
 import Hero from "@/components/Hero";
+import PupiloDelivers from "@/components/PupiloDelivers";
 import Image from "next/image";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <PupiloDelivers />
+    </>
+  );
 }
