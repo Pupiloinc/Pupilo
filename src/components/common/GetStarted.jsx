f14642f51bf99a1a34ce205518b53ddb2471c3e8
vishal <vishalchanda2018@gmail.com>
2025-06-18 14:45:26 +0530

    get started section done

--- src/components/common/GetStarted.jsx
+++ src/components/common/GetStarted.jsx
@@ -2,11 +2,33 @@ import React from "react";
 import Heading from "./Heading";
 import Paragraph from "./Paragraph";
 import CustomButton from "./CustomButton";
+import Image from "next/image";
 
 const GetStarted = () => {
   return (
     <div className="container max-w-[1140px] xl:px-0 px-6 mx-auto py-[60px] sm:py-[100px] lg:py-40">
-      <div className=" bg-purple rounded-3xl min-h-auto sm:min-h-[352px] py-10 sm:py-[52px] max-sm:px-4">
+      <div className=" bg-purple rounded-3xl min-h-auto sm:min-h-[352px] py-10 sm:py-[52px] max-sm:px-4 relative">
+        <Image
+          src="/assets/images/png/get-started-right-icon.png"
+          alt="bg"
+          className="absolute pointer-events-none right-0 top-0 max-lg:hidden"
+          width={270}
+          height={208}
+        />
+        <Image
+          src="/assets/images/png/get-started-top-icon.png"
+          alt="bg"
+          className="absolute pointer-events-none left-[7%] top-0 max-lg:hidden"
+          width={450}
+          height={208}
+        />
+        <Image
+          src="/assets/images/png/get-started-bottom-icon.png"
+          alt="bg"
+          className="absolute pointer-events-none left-0 bottom-0 max-lg:hidden"
+          width={330}
+          height={100}
+        />
         <Heading
           className={"text-center text-white max-w-[573px] mx-auto"}
           text={"Start Your Child’s Coding Journey Today!"}
