7ab9fd99f5c90725c688e88a6e9acdd1d0eb72af
Ritik <rvjaat25@gmail.com>
2025-06-18 14:17:34 +0530

    code merge

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
-import CustomButton from "@/components/common/CustomButton";
-import Header from "@/components/common/Header";
 import Hero from "@/components/Hero";
 import PupiloDelivers from "@/components/PupiloDelivers";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
-      <CustomButton text={"Try a Free Lesson"} />
       <PupiloDelivers />
     </>
   );
