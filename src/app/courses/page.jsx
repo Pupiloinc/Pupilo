28d821351fe2099a5fe3832a8fbda76d216f63ef
vishal <vishalchanda2018@gmail.com>
2025-06-19 14:23:04 +0530

    merge with home

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,15 +1,14 @@
-
-import PupiloCurriculum from '@/components/courses/PupiloCurriculum'
-import TechJourney from '@/components/courses/TechJourney'
-import React from 'react'
+import PupiloCurriculum from "@/components/courses/pupiloCurriculum";
+import TechJourney from "@/components/courses/TechJourney";
+import React from "react";
 
 const page = () => {
-    return (
-        <div>
-           <PupiloCurriculum/>
-            <TechJourney/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <PupiloCurriculum />
+      <TechJourney />
+    </div>
+  );
+};
 
-export default page
+export default page;
