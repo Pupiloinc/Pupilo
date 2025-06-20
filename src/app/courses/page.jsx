bb9fd5f594ac87d3f62ad315482bc6bee54beadc
rinklepoonia <rinklepoonia889@gmail.com>
2025-06-20 10:00:17 +0530

    add-components to pages

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,12 +1,24 @@
-import PupiloCurriculum from "@/components/courses/pupiloCurriculum";
+import BackToTop from "@/components/common/BackToTop";
+import Footer from "@/components/common/Footer";
+import GetStarted from "@/components/common/GetStarted";
+import Reviews from "@/components/common/Reviews";
+import CourseHero from "@/components/courses/CourseHero";
+import PupiloCurriculum from "@/components/courses/PupiloCurriculum";
 import TechJourney from "@/components/courses/TechJourney";
+import BrandMarquee from "@/components/home/BrandMarquee";
 import React from "react";
 
 const page = () => {
   return (
     <div>
+      <CourseHero />
+      <BrandMarquee />
       <PupiloCurriculum />
       <TechJourney />
+      <Reviews />
+      <GetStarted />
+      <Footer />
+      <BackToTop />
     </div>
   );
 };
