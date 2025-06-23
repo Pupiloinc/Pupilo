b6823a83a18286e22377c760edaafdfc6433159a
Ritik <rvjaat25@gmail.com>
2025-06-23 10:15:39 +0530

    responsive done

--- src/components/common/JoinTheWaitList.jsx
+++ src/components/common/JoinTheWaitList.jsx
@@ -52,43 +52,43 @@ const JoinTheWaitList = () => {
         return () => clearInterval(timer);
     }, []);
     return (
-        <div className='min-h-screen bg-purple flex flex-col justify-center px-4 sm:px-6'>
-            <h2 className='text-center text-white font-semibold text-custom-6xl leading-120 max-w-[936px] mx-auto'>Join the Waitlist.
+        <div className='min-h-screen bg-purple flex flex-col justify-center px-4 sm:px-6 max-sm:py-12'>
+            <h2 className='text-center text-white font-semibold text-3xl sm:text-4xl lg:text-custom-6xl leading-120 max-w-[936px] mx-auto'>Join the Waitlist.
                 <br />Empower a Future Innovator.</h2>
-            <p className='font-normal text-base leading-150 text-white opacity-80 max-w-[771px] text-center mx-auto mt-3'>At HelpBnk, we believe that collective wisdom is the foundation of growth. By connecting individuals, entrepreneurs, and businesses in a collaborative ecosystem, we empower you to share insights, gain valuable expertise, and accelerate success.</p>
-            <p className='font-semibold text-custom-4xl leading-120 text-[#F1F1F1] text-center mt-12'>Get Notified Soon</p>
+            <p className='font-normal text-sm md:text-base leading-150 text-white opacity-80 max-w-[771px] text-center mx-auto mt-3'>At HelpBnk, we believe that collective wisdom is the foundation of growth. By connecting individuals, entrepreneurs, and businesses in a collaborative ecosystem, we empower you to share insights, gain valuable expertise, and accelerate success.</p>
+            <p className='font-semibold text-2xl md:text-custom-4xl leading-120 text-[#F1F1F1] text-center mt-8 lg:mt-12'>Get Notified Soon</p>
             <form className='flex flex-column justify-center'>
-                <div className="w-full max-w-[698px] mt-3 flex gap-2">
-                    <div className="max-w-[491px] w-full bg-white rounded-full py-3.5 px-8">
-                        <input required type="email" placeholder='Enter your Email' className='font-semibold text-base leading-100 placeholder:text-dark-grey outline-none border-none' />
+                <div className="w-full max-w-[698px] mt-3 flex gap-2 max-lg:justify-center">
+                    <div className="lg:max-w-[491px] max-w-[375px] max-lg:justify-center w-full bg-white rounded-full py-2.5 md:py-3.5 max-lg:pl-3 lg:px-8">
+                        <input required type="email" placeholder='Enter your Email' className='font-semibold text-sm md:text-base leading-100 placeholder:text-dark-grey outline-none border-none' />
                     </div>
                     <CustomButton text="Submit" />
                 </div>
             </form>
             <div className="flex gap-4 items-center justify-center mt-3">
-                <Image src="/assets/images/webp/notified-profile.webp" width={150} height={40} alt='notified-profile' />
-                <p className='font-semibold text-white text-base leading-100'>Joined 150+ others on the waitlist</p>
+                <Image className='max-sm:h-[36px]' src="/assets/images/webp/notified-profile.webp" width={150} height={40} alt='notified-profile' />
+                <p className='font-semibold text-white text-sm lg:text-base leading-100'>Joined 150+ others on the waitlist</p>
             </div>
-            <div className="flex flex-col items-center mt-12">
-                <div className="flex gap-6 text-white text-custom-4xl font-semibold">
+            <div className="flex flex-col items-center mt-6 lg:mt-12">
+                <div className="flex gap-2 md:gap-6 text-white text-custom-4xl font-semibold max-lg:items-baseline">
                     <div className="text-center">
-                        <div className='font-semibold text-5xl leading-120 text-white'>{timeLeft.days.toString().padStart(2, '0')}</div>
-                        <div className="font-semibold text-2xl leading-120 text-white mt-5">Days</div>
+                        <div className='font-semibold text-2xl sm:text-custom-4xl md:text-5xl leading-120 text-white'>{timeLeft.days.toString().padStart(2, '0')}</div>
+                        <div className="font-semibold text-xl md:text-2xl leading-120 text-white mt-3 md:mt-5">Days</div>
                     </div>
                     <div>:</div>
                     <div className="text-center">
-                        <div className='font-semibold text-5xl leading-120 text-white'>{timeLeft.hours.toString().padStart(2, '0')}</div>
-                        <div className="font-semibold text-2xl leading-120 text-white mt-5">Hours</div>
+                        <div className='font-semibold text-2xl sm:text-custom-4xl md:text-5xl leading-120 text-white'>{timeLeft.hours.toString().padStart(2, '0')}</div>
+                        <div className="font-semibold text-xl md:text-2xl leading-120 text-white mt-3 md:mt-5">Hours</div>
                     </div>
                     <div>:</div>
                     <div className="text-center">
-                        <div className='font-semibold text-5xl leading-120 text-white'>{timeLeft.minutes.toString().padStart(2, '0')}</div>
-                        <div className="font-semibold text-2xl leading-120 text-white mt-5">Minutes</div>
+                        <div className='font-semibold text-2xl sm:text-custom-4xl md:text-5xl leading-120 text-white'>{timeLeft.minutes.toString().padStart(2, '0')}</div>
+                        <div className="font-semibold text-xl md:text-2xl leading-120 text-white mt-3 md:mt-5">Minutes</div>
                     </div>
                     <div>:</div>
                     <div className="text-center">
-                        <div className='font-semibold text-5xl leading-120 text-white'>{timeLeft.seconds.toString().padStart(2, '0')}</div>
-                        <div className="font-semibold text-2xl leading-120 text-white mt-5">Seconds</div>
+                        <div className='font-semibold text-2xl sm:text-custom-4xl md:text-5xl leading-120 text-white'>{timeLeft.seconds.toString().padStart(2, '0')}</div>
+                        <div className="font-semibold text-xl md:text-2xl leading-120 text-white mt-3 md:mt-5">Seconds</div>
                     </div>
                 </div>
             </div>
