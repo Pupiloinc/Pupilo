a24e7f1600e60330b5cbef2dcf1fbfc86d85af9e
Ritik <rvjaat25@gmail.com>
2025-07-07 11:21:55 +0530

    changes in meta img

--- src/app/contact-form/page.jsx
+++ src/app/contact-form/page.jsx
@@ -1,10 +1,14 @@
+import Footer from '@/components/common/Footer'
+import Header from '@/components/common/Header'
 import ContactForm from '@/components/contact-form/ContactForm'
 import React from 'react'
 
 const page = () => {
   return (
     <>
-        <ContactForm/>
+      <Header />
+      <ContactForm />
+      <Footer />
     </>
   )
 }
