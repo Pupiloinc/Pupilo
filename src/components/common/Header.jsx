6f9b53a8bdfa87f09dbaf2cc095410722a8d9787
Hulisani351 <trevorrambau10@gmail.com>
2026-03-02 17:51:29 +0200

    yes

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -17,10 +17,10 @@ const Header = () => {
         if (isOpen) {
             document.body.style.overflow = "hidden";
         } else {
-            document.body.style.overflow = "auto";
+            document.body.style.overflow = "";
         }
         return () => {
-            document.body.style.overflow = "auto";
+            document.body.style.overflow = "";
         };
     }, [isOpen]);
     return (
